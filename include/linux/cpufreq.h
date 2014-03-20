6836546ea73ca306edcccea1fb55ad19bd89c2be
Chester Kener <Cl3Kener@gmail.com>
2014-03-20 12:47:41 -0400

    cpufreq: Add Lazy
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    
    Conflicts:
    	arch/arm/configs/deathly_d2_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -431,6 +431,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
+extern struct cpufreq_governor cpufreq_gov_lazy;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lazy)
 #endif
 
 
