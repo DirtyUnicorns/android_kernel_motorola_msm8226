ddd4f9330902563462ccc6219339cdd8e586d9f6
savoca <adeddo27@gmail.com>
2014-07-26 15:08:01 +0000

    s2w: Use s2s-only (s2w_switch == 2) properly

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -3688,7 +3688,7 @@ static int synaptics_rmi4_suspend(struct device *dev)
 			rmi4_data->board;
 
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-	if ((s2w_switch > 0) || (dt2w_switch > 0)) {
+	if ((s2w_switch == 1) || (dt2w_switch)) {
 		pr_info("suspend avoided!\n");
 		return 0;
 	} else {
